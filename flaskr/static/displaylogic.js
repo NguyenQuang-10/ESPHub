d0bc80592ff7c289aa02d6d99c6ccb1d2f1003f1
Nguyen Nhat Quang <nguyennhatquang143@gmail.com>
2021-08-01 20:22:17 +0700

    Add node function half-way done

--- flaskr/static/displaylogic.js
+++ flaskr/static/displaylogic.js
@@ -5,6 +5,15 @@ function getWindowSize(){
     return [x,y];
 }
 
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+
+
+
 function updateLayout(){
     var size = getWindowSize();
     var x = size[0];
@@ -34,9 +43,56 @@ socketio.on("init", function(nodestr){
     document.getElementById("hostIP").innerHTML = nodejson["platformdata"]["ip"]
 })
 
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
 
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus()
+// })
 
-function testWS(){
-    console.log("Big Penis");
-    socketio.emit('eventbigpenis', "Pemis");
+var channelNumTracker = 0;
+
+$('#newnodemodal').on('hide.bs.modal', function () {
+    channelNumTracker = 0;
+    var chnform = document.getElementById("chnform");
+    chnform.innerHTML = ""
+})
+
+function addChannel(){
+
+    // <h5>Channel <span id="chnnum"></span></h5>
+    // <label for="chnname">Channel Name</label> <br>
+    // <input type="text" maxlength="50" id="chnname" name="chnname"><br><br>
+    // <label for="chninp">Channel Input Type: </label> <br>
+    // <select id="chninp" name="chninp">
+    //     <option value="switch">Switch</option>
+    //     <option value="variable">Slider</option>
+    // </select><br><br>
+    // <label for="chndesc">Channel Description</label>
+    // <input type="text" maxlength="200" id="chndesc" name="chndesc"><br><br></br>
+    
+    channelNumTracker += 1;
+    var chnform = document.getElementById("chnform");
+    // new html for channel form, i dont know how to use format string lmao
+    var cfHtmlString = '<div><h5>Channel ' + channelNumTracker.toString() + ' </h5> \
+    <label for="chn' + channelNumTracker.toString() + 'name">Channel Name:</label> <br> \
+    <input type="text" maxlength="50" id="chn' + channelNumTracker.toString() + 'name" name="chn' + channelNumTracker.toString() + 'name"><br><br> \
+    <label for="chn' + channelNumTracker.toString() + 'inp">Channel Input Type: </label> <br> \
+    <select id="chn' + channelNumTracker.toString() + 'inp" name="chn' + channelNumTracker.toString() + 'inp"> \
+        <option value="switch">Switch</option> \
+        <option value="variable">Slider</option> \
+    </select><br><br> \
+    <label for="chn' + channelNumTracker.toString + 'desc">Channel Description:</label> \
+    <input type="text" maxlength="200" id="chn' + channelNumTracker.toString + 'desc" name="chn' + channelNumTracker.toString + 'desc"><br><br></div>'
+    
+    var cfclone = htmlToElement(cfHtmlString);
+
+    chnform.appendChild(cfclone);
+
+}
+
+function removeChannel(){
+    var chnform = document.getElementById("chnform");
+    chnform.removeChild(chnform.lastChild);
+    channelNumTracker -= 1;
 }
